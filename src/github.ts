d54f913bd9055d4cb90113e9620d48d4534dac68
3Shain <san3shain@outlook.com>
2023-03-08 03:46:29 +0000

    refactor: change a proxy domain and do octocat test 3 times

--- src/github.ts
+++ src/github.ts
@@ -1,24 +1,33 @@
 import { log, timeout } from "./utils";
 
-const END_POINTS = ["", "https://gh.3ss.workers.dev/"];
+const END_POINTS = ["", "https://ghp.3shain.uk/"];
 
 export async function createGithubEndpoint() {
   await log(`Checking github endpoints`);
-  const fastest = await Promise.race(
-    [...END_POINTS.map((prefix) =>
-      fetch(`${prefix}https://api.github.com/octocat`).then(x=>x.text()).then(x=>prefix)
-    ),timeout(1000)]
-  );
+  const fastest = await Promise.race([
+    ...END_POINTS.map((prefix) =>
+      fetch(`${prefix}https://api.github.com/octocat`)
+        .then((x) => x.text())
+        .then(() => fetch(`${prefix}https://api.github.com/octocat`))
+        .then((x) => x.text())
+        .then(() => fetch(`${prefix}https://api.github.com/octocat`))
+        .then((x) => x.text())
+        .then((x) => prefix)
+    ),
+    timeout(20000),
+  ]);
 
   fastest == "" || (await log(`Using github proxy ${fastest}`));
 
   function api(path: `/${string}`): Promise<any> {
-    return fetch(`${fastest}https://api.github.com${path}`).then(x=>{
-      if(x.status==200||x.status==301||x.status==302) {
-        return x.json()
+    return fetch(`${fastest}https://api.github.com${path}`).then((x) => {
+      if (x.status == 200 || x.status == 301 || x.status == 302) {
+        return x.json();
       }
-      return Promise.reject(new Error(`Request failed: ${x.status} ${x.statusText} (${x.url})`));
-    })
+      return Promise.reject(
+        new Error(`Request failed: ${x.status} ${x.statusText} (${x.url})`)
+      );
+    });
   }
 
   function acceleratedPath(path: string) {
@@ -27,7 +36,7 @@ export async function createGithubEndpoint() {
 
   return {
     api,
-    acceleratedPath
+    acceleratedPath,
   };
 }
 
