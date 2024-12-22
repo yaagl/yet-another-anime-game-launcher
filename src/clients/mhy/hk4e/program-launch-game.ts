2d53ee01f4167e082356af3ad0b9595ff7300157
3Shain <san3shain@outlook.com>
2024-12-22 20:29:53 +0800

    fix: workaround 10351-4001 for all servers

--- src/clients/mhy/hk4e/program-launch-game.ts
+++ src/clients/mhy/hk4e/program-launch-game.ts
@@ -41,9 +41,7 @@ ${await (async () => {
   } else {
     return `"${wine.toWinePath(join(gameDir, gameExecutable))}"${
       /* workaround 10351-4001 */
-      server.id == "hk4e_cn"
-        ? " -platform_type CLOUD_THIRD_PARTY_PC -is_cloud 1"
-        : ""
+      " -platform_type CLOUD_THIRD_PARTY_PC -is_cloud 1"
     }`;
   }
 })()}`;
