28044112d79066d6e7dace83d0e9f2ffe197c973
Nikuno Menura <60579848+FOREVEREALIZE@users.noreply.github.com>
2025-08-07 20:51:53 +0200

    Run Prettier and swap the sidecar binary names so they make more sense
    Signed-off-by: foreverealize! <60579848+FOREVEREALIZE@users.noreply.github.com>

--- src/clients/mhy/patch.ts
+++ src/clients/mhy/patch.ts
@@ -114,10 +114,22 @@ export async function* patchProgram(
     );
   }
 
-  await cp(resolve("./sidecar/protonextras/steam32.exe"), join(system32Dir, "steam.exe"))
-  await cp(resolve("./sidecar/protonextras/steam64.exe"), join(syswow64Dir, "steam.exe"))
-  await cp(resolve("./sidecar/protonextras/lsteamclient32.dll"), join(system32Dir, "lsteamclient.dll"))
-  await cp(resolve("./sidecar/protonextras/lsteamclient64.dll"), join(syswow64Dir, "lsteamclient.dll"))
+  await cp(
+    resolve("./sidecar/protonextras/steam64.exe"),
+    join(system32Dir, "steam.exe")
+  );
+  await cp(
+    resolve("./sidecar/protonextras/steam32.exe"),
+    join(syswow64Dir, "steam.exe")
+  );
+  await cp(
+    resolve("./sidecar/protonextras/lsteamclient64.dll"),
+    join(system32Dir, "lsteamclient.dll")
+  );
+  await cp(
+    resolve("./sidecar/protonextras/lsteamclient32.dll"),
+    join(syswow64Dir, "lsteamclient.dll")
+  );
 
   setKey("patched", "1");
 }
