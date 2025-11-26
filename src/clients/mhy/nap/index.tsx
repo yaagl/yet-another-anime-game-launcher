95e83a5372ebb4c5a8123b8395c04fe34c3b7b27
ScathachSkadi <70437676+ScathachSkadi@users.noreply.github.com>
2025-11-26 08:34:53 +0000

    chore: nap 2.4.0

--- src/clients/mhy/nap/index.tsx
+++ src/clients/mhy/nap/index.tsx
@@ -44,7 +44,7 @@ import {
 } from "../launcher-info";
 import { getLatestAdvInfo, getLatestVersionInfo } from "../hyp-connect";
 
-const CURRENT_SUPPORTED_VERSION = "2.3.0";
+const CURRENT_SUPPORTED_VERSION = "2.4.0";
 
 export async function getGameVersion(gameDataDir: string, offset: number) {
   const ret = await _getGameVersion(gameDataDir, offset);
