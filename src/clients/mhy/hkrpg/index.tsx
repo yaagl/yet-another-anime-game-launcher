7e5cdf3ce418de051dfb02674e07cd7a4318fd38
ScathachSkadi <70437676+ScathachSkadi@users.noreply.github.com>
2025-12-20 14:40:03 +0000

    chore: hkrpg 3.8.0

--- src/clients/mhy/hkrpg/index.tsx
+++ src/clients/mhy/hkrpg/index.tsx
@@ -44,7 +44,7 @@ import createPatchOff from "./config/patch-off";
 import createBlockNet from "./config/block-net";
 import { getLatestAdvInfo, getLatestVersionInfo } from "../hyp-connect";
 
-const CURRENT_SUPPORTED_VERSION = "3.7.0";
+const CURRENT_SUPPORTED_VERSION = "3.8.0";
 
 export async function createHKRPGChannelClient({
   server,
