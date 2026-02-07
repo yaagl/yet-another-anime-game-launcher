92bdcdbf89705025bb2bd03cdcaa89a6472a4b8f
Balthild <ibalthild@gmail.com>
2026-02-06 20:02:15 -0600

    refactor: write async iterator as a generator

--- src/utils/concurrent-tasks.ts
+++ src/utils/concurrent-tasks.ts
@@ -1,41 +1,29 @@
-export class ConcurrentTasks<T, R> {
-  private readonly limit: number;
-  private readonly inputs: T[];
-  private readonly work: (input: T) => Promise<R>;
-
+export class ConcurrentTasks<T, R> implements AsyncIterable<R> {
   public constructor(
-    limit: number,
-    inputs: T[],
-    work: (input: T) => Promise<R>
-  ) {
-    this.limit = limit;
-    this.inputs = inputs;
-    this.work = work;
-  }
+    private readonly limit: number,
+    private readonly inputs: T[],
+    private readonly work: (input: T) => Promise<R>
+  ) {}
 
-  [Symbol.asyncIterator]() {
-    let index = 0;
+  public async *[Symbol.asyncIterator](): AsyncIterator<R> {
+    const running = this.inputs
+      .slice(0, this.limit)
+      .map(input => this.work(input));
 
-    const running: Promise<R>[] = [];
-    for (let i = 0; i < this.limit && i < this.inputs.length; i++) {
-      const input = this.inputs[index++];
-      running.push(this.work(input));
+    let index = running.length;
+
+    while (true) {
+      const promise = running.shift();
+      if (!promise) {
+        break;
+      }
+
+      if (index < this.inputs.length) {
+        const input = this.inputs[index++];
+        running.push(this.work(input));
+      }
+
+      yield await promise;
     }
-
-    return {
-      next: async (): Promise<IteratorResult<R>> => {
-        const promise = running.shift();
-        if (!promise) {
-          return { done: true, value: undefined };
-        }
-
-        if (index < this.inputs.length) {
-          const input = this.inputs[index++];
-          running.push(this.work(input));
-        }
-
-        return { done: false, value: await promise };
-      },
-    };
   }
 }
