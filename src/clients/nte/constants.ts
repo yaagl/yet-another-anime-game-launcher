d45234ebd78fc79bf21cc6573a18db7974307caf
shakiragas <shakiragasc4@gmail.com>
2026-07-11 14:36:50 +0300

    Add NTE launcher constants

---
+++ src/clients/nte/constants.ts
@@ -0,0 +1,7 @@
+export const VERSION_INFO_URL =
+  "https://ntecdn1.perfectworld.com/hd/publish_PC/launcher/Version.ini";
+
+export const BACKUP_VERSION_INFO_URL =
+  "https://ntecdn2.perfectworld.com/hd/publish_PC/launcher/Version.ini";
+
+export const GAME_ID = 3000001;
